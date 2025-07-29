c91701fd62f90686f0204a1403222c072f2a69b6
BideABiere-dev <108137216+BideABiere-dev@users.noreply.github.com>
2025-07-29 14:42:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const supabaseUrl = "https://qxyvxsennasbxzwhluky.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4eXZ4c2VubmFzYnh6d2hsdWt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTEwOTcsImV4cCI6MjA2OTAyNzA5N30.2ZeSzacrYH-3tEqqvezBbovvJrxlazbLvO6vZDgjEQE";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 function navigateTo(pageId) {
   document.querySelectorAll(".page").forEach((page) => {
